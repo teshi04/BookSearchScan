4012c315b08f1fa7664490655ffdbb914d71be5f
teshi04 <teshi04@gmail.com>
2015-02-26 03:05:14 +0900

    プログレスバーを表示

--- app/src/main/java/jp/tsur/chil/ui/ItemActivity.java
+++ app/src/main/java/jp/tsur/chil/ui/ItemActivity.java
@@ -7,28 +7,56 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import jp.tsur.chil.BuildConfig;
 import jp.tsur.chil.R;
+import jp.tsur.chil.api.AwsApi;
+import jp.tsur.chil.api.AwsService;
+import jp.tsur.chil.model.Author;
+import jp.tsur.chil.model.Item;
+import jp.tsur.chil.model.ItemAttributes;
+import jp.tsur.chil.model.ItemLookupResponse;
+import jp.tsur.chil.utils.Utils;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class ItemActivity extends ActionBarActivity {
 
-    public static final String EXTRA_ITEM_TITLE = "book_title";
-    public static final String EXTRA_ITEM_AUTHOR = "book_author";
-    public static final String EXTRA_ITEM_URL = "book_url";
-    public static final String EXTRA_EXISTS_KINDLE = "book_exists_kindle";
-    public static final String URL_CHIL_CHIL = "http://www.chil-chil.net/sp/goodsList/?freeword=";
+    private static final String AMAZON_URL = "GET\necs.amazonaws.jp\n/onca/xml\n";
+    private static final String AMAZON_VERSION = "2011-08-01";
+    private static final String URL_CHIL_CHIL = "http://www.chil-chil.net/sp/goodsList/?freeword=";
+    public static final String EXTRA_ISBN = "isbn";
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
+    @InjectView(R.id.progress_bar)
+    ProgressBar progressBar;
+    @InjectView(R.id.card_view)
+    CardView cardView;
     @InjectView(R.id.title_view)
     TextView titleView;
     @InjectView(R.id.author_view)
@@ -56,12 +84,18 @@ public class ItemActivity extends ActionBarActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        Bundle extras = getIntent().getExtras();
-        amazonUrl = extras.getString(EXTRA_ITEM_URL);
-        title = extras.getString(EXTRA_ITEM_TITLE);
+        Intent intent = getIntent();
+        if (intent != null) {
+            search(intent.getExtras().getString(EXTRA_ISBN));
+        }
+    }
+
+    private void setResult(String title, String author, String amazonUrl, boolean kindleExist) {
+        this.title = title;
+        this.amazonUrl = amazonUrl;
         titleView.setText(title);
-        authorView.setText(extras.getString(EXTRA_ITEM_AUTHOR));
-        if (extras.getBoolean(EXTRA_EXISTS_KINDLE)) {
+        authorView.setText(author);
+        if (kindleExist) {
             kindleExistView.setVisibility(View.VISIBLE);
         } else {
             kindleNoneView.setVisibility(View.VISIBLE);
@@ -70,7 +104,79 @@ public class ItemActivity extends ActionBarActivity {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         if (sharedPref.getBoolean("chilchil_visible", false))
             openChilButton.setVisibility(View.VISIBLE);
+
+        progressBar.setVisibility(View.GONE);
+        cardView.setVisibility(View.VISIBLE);
     }
+
+    private void search(final String isbn) {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.JAPAN);
+        String timestamp = df.format(new Date());
+
+        ArrayList<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("AWSAccessKeyId", getString(R.string.aws_access_key_id)));
+        params.add(new BasicNameValuePair("AssociateTag", getString(R.string.amazon_associate_tag)));
+        params.add(new BasicNameValuePair("IdType", "ISBN"));
+        params.add(new BasicNameValuePair("ItemId", isbn));
+        params.add(new BasicNameValuePair("Operation", "ItemLookup"));
+        params.add(new BasicNameValuePair("ResponseGroup", "ItemAttributes"));
+        params.add(new BasicNameValuePair("SearchIndex", "Books"));
+        params.add(new BasicNameValuePair("Service", "AWSECommerceService"));
+        params.add(new BasicNameValuePair("Timestamp", timestamp));
+        params.add(new BasicNameValuePair("Version", AMAZON_VERSION));
+        String target = AMAZON_URL + Utils.getQuery(params);
+        String digest = Utils.toHmacSHA256(target, getString(R.string.aws_secret_access_key_id));
+        digest = Utils.urlEncode(digest);
+
+        AwsApi api = AwsService.getAwsService();
+        api.getBook(getString(R.string.aws_access_key_id), getString(R.string.amazon_associate_tag), "ISBN", isbn,
+                "ItemLookup", "ItemAttributes", "Books", "AWSECommerceService",
+                timestamp, AMAZON_VERSION, digest, new Callback<ItemLookupResponse>() {
+                    @Override
+                    public void success(ItemLookupResponse itemLookupResponse, Response response) {
+                        if (itemLookupResponse.getItems() == null) {
+                            Toast.makeText(ItemActivity.this, "ISBN コードが間違っています", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        List<Item> itemList = itemLookupResponse.getItems().getItemList();
+                        String title = "";
+                        String authorList = "";
+                        String url = "";
+
+                        boolean existsKindle = false;
+                        for (Item item : itemList) {
+                            ItemAttributes itemAttributes = item.getItemAttributes();
+                            if (itemAttributes.getBinding().contains("Kindle")) {
+                                existsKindle = true;
+                            } else {
+                                title = itemAttributes.getTitle();
+                                url = item.getDetailPageURL();
+                                for (Author author : itemAttributes.getAuthorList()) {
+                                    authorList = TextUtils.isEmpty(authorList) ?
+                                            author.getAuthorName() : authorList + ", " + author.getAuthorName();
+                                }
+                            }
+                        }
+
+                        setResult(title, authorList, url, existsKindle);
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        if (BuildConfig.DEBUG) {
+                            Log.d("bbs", error.getResponse().getUrl());
+                            error.printStackTrace();
+                        }
+                        if (error.getResponse() == null) {
+                            Toast.makeText(ItemActivity.this, getString(R.string.toast_error_net), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(ItemActivity.this, getString(R.string.toast_error_other), Toast.LENGTH_SHORT).show();
+                        }
+                        finish();
+                    }
+                });
+    }
+
 
     @OnClick(R.id.open_chil_button)
     void openChilChil() {
--- app/src/main/java/jp/tsur/chil/ui/MainActivity.java
+++ app/src/main/java/jp/tsur/chil/ui/MainActivity.java
@@ -4,41 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import jp.tsur.chil.R;
-import jp.tsur.chil.api.AwsApi;
-import jp.tsur.chil.api.AwsService;
-import jp.tsur.chil.model.Author;
-import jp.tsur.chil.model.Item;
-import jp.tsur.chil.model.ItemAttributes;
-import jp.tsur.chil.model.ItemLookupResponse;
-import jp.tsur.chil.utils.Utils;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 
 public class MainActivity extends ActionBarActivity {
-
-    private static final String AMAZON_URL = "GET\necs.amazonaws.jp\n/onca/xml\n";
-    private static final String AMAZON_VERSION = "2011-08-01";
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
@@ -63,79 +38,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 0 && resultCode == RESULT_OK) {
             String isbn = data.getStringExtra("SCAN_RESULT");
-            search(isbn);
+
+            Intent intent = new Intent(MainActivity.this, ItemActivity.class);
+            intent.putExtra(ItemActivity.EXTRA_ISBN, isbn);
+            startActivity(intent);
         }
-    }
-
-    private void search(final String isbn) {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.JAPAN);
-        String timestamp = df.format(new Date());
-
-        ArrayList<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("AWSAccessKeyId", getString(R.string.aws_access_key_id)));
-        params.add(new BasicNameValuePair("AssociateTag", getString(R.string.amazon_associate_tag)));
-        params.add(new BasicNameValuePair("IdType", "ISBN"));
-        params.add(new BasicNameValuePair("ItemId", isbn));
-        params.add(new BasicNameValuePair("Operation", "ItemLookup"));
-        params.add(new BasicNameValuePair("ResponseGroup", "ItemAttributes"));
-        params.add(new BasicNameValuePair("SearchIndex", "Books"));
-        params.add(new BasicNameValuePair("Service", "AWSECommerceService"));
-        params.add(new BasicNameValuePair("Timestamp", timestamp));
-        params.add(new BasicNameValuePair("Version", AMAZON_VERSION));
-
-        String target = AMAZON_URL + Utils.getQuery(params);
-        String digest = Utils.toHmacSHA256(target, getString(R.string.aws_secret_access_key_id));
-        digest = Utils.urlEncode(digest);
-
-        AwsApi api = AwsService.getAwsService();
-        api.getBook(getString(R.string.aws_access_key_id), getString(R.string.amazon_associate_tag), "ISBN", isbn,
-                "ItemLookup", "ItemAttributes", "Books", "AWSECommerceService",
-                timestamp, AMAZON_VERSION, digest, new Callback<ItemLookupResponse>() {
-                    @Override
-                    public void success(ItemLookupResponse itemLookupResponse, Response response) {
-                        if (itemLookupResponse.getItems() == null) {
-                            Toast.makeText(MainActivity.this, "ISBN コードが間違っています", Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                        List<Item> itemList = itemLookupResponse.getItems().getItemList();
-                        String title = "";
-                        String authorList = "";
-                        String url = "";
-
-                        boolean kindleExist = false;
-                        for (Item item : itemList) {
-                            ItemAttributes itemAttributes = item.getItemAttributes();
-                            if (itemAttributes.getBinding().contains("Kindle")) {
-                                kindleExist = true;
-                            } else {
-                                title = itemAttributes.getTitle();
-                                url = item.getDetailPageURL();
-                                for (Author author : itemAttributes.getAuthorList()) {
-                                    authorList = TextUtils.isEmpty(authorList) ?
-                                            author.getAuthorName() : authorList + ", " + author.getAuthorName();
-                                }
-                            }
-                        }
-
-                        Intent intent = new Intent(MainActivity.this, ItemActivity.class);
-                        intent.putExtra(ItemActivity.EXTRA_ITEM_TITLE, title);
-                        intent.putExtra(ItemActivity.EXTRA_ITEM_AUTHOR, authorList);
-                        intent.putExtra(ItemActivity.EXTRA_EXISTS_KINDLE, kindleExist);
-                        intent.putExtra(ItemActivity.EXTRA_ITEM_URL, url);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void failure(RetrofitError error) {
-                        Log.d("a", error.getResponse().getUrl());
-                        error.printStackTrace();
-                        if (error.getResponse() == null) {
-                            Toast.makeText(MainActivity.this, getString(R.string.toast_error_net), Toast.LENGTH_SHORT).show();
-                            return;
-                        }
-                        Toast.makeText(MainActivity.this, getString(R.string.toast_error_other), Toast.LENGTH_SHORT).show();
-                    }
-                });
     }
 
     @Override
