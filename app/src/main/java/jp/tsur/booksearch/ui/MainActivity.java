0709dffb6959ba4d4ecde74f6bd270be96c5364f
teshi04 <teshi04@gmail.com>
2015-06-24 03:07:04 +0900

    Use Snackbar

--- app/src/main/java/jp/tsur/booksearch/ui/MainActivity.java
+++ app/src/main/java/jp/tsur/booksearch/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -38,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
+
+    @InjectView(R.id.container)
+    View container;
 
     @InjectView(R.id.history_view)
     RecyclerView historyView;
@@ -81,8 +86,9 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
                         adapter.remove(viewHolder.getAdapterPosition());
+                        Snackbar.make(container, R.string.toast_deleted, Snackbar.LENGTH_LONG)
+                                .show();
                     }
-
                 });
         swipeToDismissTouchHelper.attachToRecyclerView(historyView);
     }
