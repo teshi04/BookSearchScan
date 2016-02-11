572b6bf34d315fb9fae8ce07a000a0c079a799cf
teshi04 <teshi04@gmail.com>
2016-02-11 18:53:55 +0900

    :sparkles:

--- app/src/main/java/jp/tsur/booksearch/ui/LicenceActivity.java
+++ app/src/main/java/jp/tsur/booksearch/ui/LicenceActivity.java
@@ -3,24 +3,14 @@ package jp.tsur.booksearch.ui;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import jp.tsur.booksearch.R;
-
 public class LicenceActivity extends AppCompatActivity {
-
-    @Bind(R.id.web_view)
-    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_licence);
-        ButterKnife.bind(this);
 
         ActionBar actionBar = getSupportActionBar();
 
@@ -28,7 +18,9 @@ public class LicenceActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        WebView webView = new WebView(this);
         webView.loadUrl("file:///android_asset/license.html");
+        setContentView(webView);
     }
 
     @Override
